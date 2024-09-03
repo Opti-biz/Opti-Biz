e96b9a1b1e45696a4a7b90c399bf429f58663e17
Dylan Hodgson <Opti-biz@outlook.com>
2024-09-03 17:08:19 +0100

    end of server.js file change

--- server.js
+++ server.js
@@ -113,6 +113,7 @@ app.get("/test", (req, res) => {
 
 
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
